0bfb21638892768e5be2d5bf07d38d03bef7563e
release_the_kraken <wmichal13@wp.pl>
2022-07-29 19:58:19 +0200

    done database creation functionality

--- src/main/java/config/Configuration.java
+++ src/main/java/config/Configuration.java
@@ -1,5 +1,10 @@
 package config;
 
-public interface Configuration {
-
+public class Configuration {
+    public static final String URL= "jdbc:mysql://localhost/blog_schema";
+    public static final String USER = "admin";
+    public static final String PASSWORD =  "admin";
+    private Configuration() {
+    //I don't want any instances of the class created
+    }
 }
